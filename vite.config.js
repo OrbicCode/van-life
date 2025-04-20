cb0ba1fee70da60434e4af0e3e054a1857d8a270
James O'Kane <orbiccode@gmail.com>
2025-04-20 21:56:36 +0100

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["firebase/app", "firebase/firestore/lite"]
-    }
-  },
-  esbuild: {
-    loader: "jsx", // Ensure JSX files are properly handled
-    include: /src\/.*\.jsx?$/, // Include your source files
-  }
 })
